548bc48bf3c16a4083380a48b8930639b1415002
vikrampatil456 <vikrampatlolaa@gmail.com>
2026-03-10 12:37:39 +0530

    created a method in service for prompts

--- src/main/java/com/research/assistant/researchassistant/Controller/ResearchController.java
+++ src/main/java/com/research/assistant/researchassistant/Controller/ResearchController.java
@@ -1,11 +1,22 @@
 package com.research.assistant.researchassistant.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.research.assistant.researchassistant.model.ResearchRequest;
+import com.research.assistant.researchassistant.service.ResearchService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/research")
 @CrossOrigin(originPatterns = "*")
+@AllArgsConstructor
 public class ResearchController {
+
+    private final ResearchService researchService;
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest request) {
+        String result = researchService.processContent(request);
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/com/research/assistant/researchassistant/service/ResearchService.java
+++ src/main/java/com/research/assistant/researchassistant/service/ResearchService.java
@@ -1,4 +1,29 @@
 package com.research.assistant.researchassistant.service;
 
+import com.research.assistant.researchassistant.model.ResearchRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ResearchService {
+    public String processContent(ResearchRequest request) {
+        // build the prompt
+        // query the AI MODEL API
+        // Parse the response
+        // return response
+    }
+    private String buildPrompt(ResearchRequest request) {
+        StringBuilder prompt = new StringBuilder();
+        switch (request.getOperation()) {
+            case "summarize":
+            prompt.append("provide a clear and concise summary of the following text in a few sentences:\n\n");
+            break;
+            case "suggest":
+                prompt.append("Based on the following content: suggest related topics and further reading. Format thr response with clear headings and bullet points:\n\n");
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown Operation: " + request.getOperation());
+        }
+        prompt.append(request.getContent());
+        return prompt.toString();
+    }
 }
